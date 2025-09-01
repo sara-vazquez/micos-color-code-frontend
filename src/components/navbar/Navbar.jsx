baa99a4e100f6938ad0a0e7350ddec462e86d42b
Sara Vázquez <saravazlo@gmail.com>
2025-09-01 20:51:07 +0200

    feat-navbar structure created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,25 @@
-import React from 'react'
-import './Navbar.css'
+import React from 'react';
+import './Navbar.css';
+import Logo from '../../assets/micosLogo.svg';
+import Menu from './Menu';
+import { Link } from 'react-router-dom'
 
+
+export default function Navbar() {
+    return(
+        <>
+        <header className= "navbar">
+            <div className= "navbar__container">
+                <div className= "logo__container">
+                    <Link to="/">
+                    <img src={Logo} alt="logotype" className="logo"></img>
+                    </Link>
+                </div>
+                <div className="navbar__buttons">
+                <Profile to="/profile" className="profile__button"/>
+                <Menu className="menu__button"/>
+                </div>
+            </div>
+        </header></>
+    );
+}

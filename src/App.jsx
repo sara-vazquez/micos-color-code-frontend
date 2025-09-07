607d63ff1f7561457359d417392b8520873360e7
Sara Vázquez <saravazlo@gmail.com>
2025-09-07 19:13:22 +0200

    fix: fixed buttons container with positions and rotations

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import React from 'react'
-import Button from './components/buttons/Button'
 import "./components/styles/Variables.css";
 import './App.css'
-import FeedbackButton from './components/feedbackButtons/FeedbackButton';
-import ScrollUpButton from './components/scrollUpButtons/ScrollUpButton';
-import LetsPlayButton from './components/letsPlayButtons/LetsPlayButton';
-import ResourceButton from './components/resourcesButtons/ResourceButton';
-import DaltonicButton from './components/daltonicButtons/DaltonicButton';
-import Navbar from './components/navbar/Navbar'
+import HomePage from './pages/HomePage';
 
 
 function App() {
 
   return (
-    <>
-    <Navbar/>
-    <Button variant="primary">ACEPTAR</Button>
-    <Button variant="secondary">CANCELAR</Button>
-    <FeedbackButton />
-    <ScrollUpButton />
-    <LetsPlayButton />
-    <ResourceButton />
-    <DaltonicButton />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import './HomePage.css';
+import Navbar from '../components/navbar/Navbar';
+import Button from '../components/buttons/Button'
+import FeedbackButton from '../components/feedbackButtons/FeedbackButton';
+import LetsPlayButton from '../components/letsPlayButtons/LetsPlayButton';
+import ResourceButton from '../components/resourcesButtons/ResourceButton';
+import DaltonicButton from '../components/daltonicButtons/DaltonicButton';
+import Footer from '../components/footer/Footer';
 
 export default function HomePage() {
     return(
+        <>
         <Navbar />
+        <Button variant="primary">Saber más →</Button>
+        <FeedbackButton />
+        <div className="buttons__container">
+            <LetsPlayButton />
+            <ResourceButton />
+            <DaltonicButton />
+        </div>
+        
+        <Footer />
+            </>
     )
 }

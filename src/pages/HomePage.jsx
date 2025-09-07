16f4e38995f7e0dcddc051e21107420231e5cce7
Sara Vázquez <saravazlo@gmail.com>
2025-09-07 19:28:21 +0200

    fix: fixed feedback button's position

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,6 +14,7 @@ export default function HomePage() {
         <Navbar />
         <Button variant="primary">Saber más →</Button>
         <FeedbackButton />
+      
         <div className="buttons__container">
             <LetsPlayButton />
             <ResourceButton />

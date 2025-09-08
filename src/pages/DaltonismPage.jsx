131197663d0f250d1cca1d23856267808d434349
Sara Vázquez <saravazlo@gmail.com>
2025-09-08 21:35:19 +0200

    styles: some styles and structure created for daltonism page

--- src/pages/DaltonismPage.jsx
+++ src/pages/DaltonismPage.jsx
@@ -4,6 +4,15 @@ import './DaltonismPage.css';
 export default function DaltonismPage() {
     return(
         <div className="daltonism">
+            <div className="daltonism__intro">
+                <h1 className="daltonism__title">Daltonismo</h1>
+                <p className = "daltonism__text">El daltonismo afecta a 350 millones de personas, el 8% de la población, aproximadamente 1 de cada 12 hombres y 1 de cada 200 mujeres. Los primeros síntomas se detectan durante la etapa escolar, mostrando dificultades a la hora de reconocer colores.
+                
+                Con el tiempo, las limitaciones de esta patología van aumentando, afectando al abanico de posibilidades profesionales y a la realización personal.</p>
+            </div>
+            <div className="daltonism__types">
+                <h2 className="daltonism__h2">Tipos de daltonismo</h2>
+            </div>
         </div>
     )
 }

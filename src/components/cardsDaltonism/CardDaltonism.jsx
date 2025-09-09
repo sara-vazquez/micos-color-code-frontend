d938f6e5ef1b0f8dbea61d61ab225b7ac090af8a
Sara Vázquez <saravazlo@gmail.com>
2025-09-09 09:56:09 +0200

    structure: created card daltonism structure

--- src/components/cardsDaltonism/CardDaltonism.jsx
+++ src/components/cardsDaltonism/CardDaltonism.jsx
@@ -4,6 +4,11 @@ import './CardDaltonism.css';
 export default function CardDaltonism() {
     return(
         <>
+        <div className='daltonism-card__container'>
+            <h3 className='daltonism__h3'></h3>
+            <p className="daltonism-card__text"></p>
+            <img></img>
+        </div>
         </>
     )
 }

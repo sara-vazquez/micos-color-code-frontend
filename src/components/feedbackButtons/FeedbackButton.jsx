d82332ff780e76f83e8dae06111b0a28ffdc4bd4
Sara Vázquez <saravazlo@gmail.com>
2025-10-03 14:26:56 +0200

    fix: added props to feedback button to open links

--- src/components/feedbackButtons/FeedbackButton.jsx
+++ src/components/feedbackButtons/FeedbackButton.jsx
@@ -3,11 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHandPointUp } from "@fortawesome/free-regular-svg-icons"; 
 import "./FeedbackButton.css";
 
-export default function FeedbackButton() {
+export default function FeedbackButton(props) {
     return(
         <div className="feedback-wrapper">
             <div className="wavy-background"></div>
-            <button className="feedback">
+            <button className="feedback" {...props}>
                 <FontAwesomeIcon icon={faHandPointUp} size="2x" />   
             </button>
         </div>

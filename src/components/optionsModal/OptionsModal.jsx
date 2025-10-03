d55ccf98bd191e379325e296ee8d8db2c2d07d9d
Sara Vázquez <saravazlo@gmail.com>
2025-10-03 16:33:59 +0200

    feat: added functionality to feedback button with guide modal

--- src/components/optionsModal/OptionsModal.jsx
+++ src/components/optionsModal/OptionsModal.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import './OptionsModal.css';
+import InfoModal from '../infoModal/InfoModal';
 
-export default function OptionsModal({ onGoToGuide, onGoToFeedback, onClose }) {
+export default function OptionsModal({ onClose }) {
+    const [showGuide, setShowGuide] = useState(false);
+
+    if(showGuide) return <InfoModal onClose={onClose}/>
+
     return(
         <div className="options-modal__overlay" onClick={onClose}>
             <section className="options-modal" role="dialog" aria-modal="true" onClick={(e) => e.stopPropagation()}>
                 <article className="options-modal__actions">
-                    <button type="button" className="options-modal__link" onClick={onGoToGuide}>Consejos de uso</button>
-                    <button type="button" className="options-modal__link" onClick={onGoToFeedback}>¡Queremos tu feedback!</button>
+                    <button type="button" className="options-modal__link" onClick={() => setShowGuide(true)}>Consejos de uso</button>
+                    <button type="button" className="options-modal__link">¡Queremos tu feedback!</button>
                 </article>
             </section>
         </div>

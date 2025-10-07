7f852d057c86ec87c221983cc6051528ff282547
Sara Vázquez <saravazlo@gmail.com>
2025-10-07 09:55:36 +0200

    feat: scroll to top button is shown but doesnt work

--- src/pages/DaltonismPage.jsx
+++ src/pages/DaltonismPage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
 import './DaltonismPage.css';
+import FeedbackButton from '../components/feedbackButtons/FeedbackButton';
+import ScrollUpButton from '../components/scrollUpButtons/ScrollUpButton';
 import CardDaltonism from "../components/cardsDaltonism/CardDaltonism";
 import Acromatopsia from "../assets/acromatopsia.png";
 import Dicromático from "../assets/anomalias.png";
@@ -11,18 +13,18 @@ import Tritanopia from "../assets/tritanopia.png";
 
 export default function DaltonismPage() {
     return(
-        <div className="daltonism">
+        <section className="daltonism">
             <Navbar />
-            <div className="daltonism__intro">
+            <article className="daltonism__intro">
                 <h1 className="daltonism__title">Daltonismo</h1>
                 <p className = "daltonism__text">El daltonismo afecta a <strong>350 millones de personas</strong>, el 8% de la población, aproximadamente <strong>1 de cada 12 hombres y 1 de cada 200 mujeres.</strong> Los primeros síntomas se detectan durante la etapa escolar, mostrando dificultades a la hora de reconocer colores.
                 
                 Con el tiempo, las limitaciones de esta patología van aumentando, afectando al abanico de posibilidades profesionales y a la realización personal.</p>
-            </div>
-            <div className="daltonism__types">
+            </article>
+            <section className="daltonism__types">
                 <h2 className="daltonism__h2">Tipos de daltonismo</h2>
                 <p className="daltonism__text">Existen diversos tipos de daltonismo reconocidos en la actualidad, en total <strong>8 arquetipos</strong> divididos en tres grandes bloques: <strong>acromático, monocromático y el dicromático.</strong></p>
-                <div className="daltonism__cards">
+                <article className="daltonism__cards">
                     <CardDaltonism 
                     title="ACROMÁTICO"
                     text="Uno de los tipos de daltonismo más extraños y por tanto poco frecuentes que existen (1 de cada 100.000 personas). El individuo ve en blanco y negro, sin distinguir ningún color, debido bien a problemas neurológicos o bien fisiológicos en el ojo."
@@ -31,12 +33,12 @@ export default function DaltonismPage() {
                     title="DICROMÁTICO"
                     text="En este gran grupo, existen tres casos principales y tres tipos parciales. Como parciales destacan la protanomalía, que hace que el rojo se vea más verde y menos brillante, la deuteranomalía que percibe el verde más rojizo y la tritanomalía que dificulta la diferencia entre el azul y el verde, y el rojo y amarillo."
                     image={Dicromático} />
-                </div>
-            </div>
-            <div className="daltonisim__main">
+                </article>
+            </section>
+            <section>
                 <h2 className="daltonism__h2">Daltonismo dicromático. Casos principales</h2>
                 <p className="daltonism__text">Estos son los casos principales del  daltonismo dicromático, de los cuales los más comunes son la <strong> protanopia y la deuteranopia.</strong></p>
-                <div className="daltonism__cards">
+                <article className="daltonism__cards">
                 <CardDaltonism 
                 title="PROTANOPIA"
                     text="El individuo no posee los conos encargados de distinguir el rojo. Por ello, los sujetos confunden el rojo y el verde."
@@ -49,13 +51,13 @@ export default function DaltonismPage() {
                 title="TRITANOPIA"
                     text="Las personas con tritanopia no distinguen el azul y el amarillo. Dentro del daltonismo dicromático, es la menos común"
                     image={Tritanopia} />
-                </div>
-            </div>
-            
+                </article>
+                    
+                <FeedbackButton className="feedback__flying-button"/>  
+            </section>
+            <ScrollUpButton />
             
             <Footer />
-        </div>
+        </section>
     )
 }
-
-/* crear cards con tipos de daltonismo */

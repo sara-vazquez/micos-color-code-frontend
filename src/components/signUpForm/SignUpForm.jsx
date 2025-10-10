0256a0480ff6ce1065fbbf3fa6155a9f7dbaf76f
Sara Vázquez <saravazlo@gmail.com>
2025-10-10 15:28:01 +0200

    style: added sign up form styles

--- src/components/signUpForm/SignUpForm.jsx
+++ src/components/signUpForm/SignUpForm.jsx
@@ -53,10 +53,21 @@ export default function SignUpForm({onGoToLogin}) {
         return(
         <>
         <form className="signup-form" onSubmit={handleSubmit}>
-            <input className="signup-form__input" name="username" value={formData.username}  onChange={handleChange} placeholder="lisasimpson8" required/>
-            <input className="signup-form__input" name="email" value={formData.email} onChange={handleChange} placeholder="lisasimpson@gmail.com" required/>
-            <input className="signup-form__input" name="password" type="password" value={formData.password} onChange={handleChange} placeholder="UsaUnaContraseñaSegura123!" required/>
-
+            <h1 className='signup-form__title'>Regístrate</h1>
+            <section className="signup-form__input-group">
+                <article className="signup-form__input-field">
+                    <label className="signup-form__label">Nombre de usuario</label>
+                    <input className="signup-form__input" name="username" value={formData.username} onChange={handleChange} placeholder="lisasimpson8" required/>
+                </article>
+                <article className="signup-form__input-field">
+                    <label className="signup-form__label">Correo electrónico</label>
+                    <input className="signup-form__input" name="email" value={formData.email} onChange={handleChange} placeholder="lisasimpson@gmail.com" required/>
+                </article>
+                <article className="signup-form__input-field">
+                    <label className="signup-form__label">Contraseña</label>
+                    <input className="signup-form__input" name="password" type="password" value={formData.password} onChange={handleChange} placeholder="UsaUnaContraseñaSegura123!" required/>
+                </article>
+            </section>
             <Captcha onCaptchaSolved={handleCaptchaSolved} />
             
             <Button type="submit" variant="primary" disabled={loading}>{loading ? "Registrando..." : "Registrarse"}</Button>

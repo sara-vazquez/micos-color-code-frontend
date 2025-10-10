fd1274edeadd613824b0b12449c78ef8b43a4e58
Sara Vázquez <saravazlo@gmail.com>
2025-10-10 19:05:04 +0200

    feat: created profile page with styles, need to do edit modal to save changes and options modal to edit or logout

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,17 +3,26 @@ import './ProfilePage.css';
 import Navbar from '../components/navbar';
 import Button from '../components/buttons/Button';
 import Footer from '../components/footer/Footer';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisVertical, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 export default function ProfilePage() {
     return(
         <>
         <Navbar />
-        <header>
-            <h1 className="profile__title">¡Hola `${}`</h1>
+        <header className="profile__header">
+            <button className="profile__back" aria-label="botón para volver atrás">
+                <FontAwesomeIcon icon={faArrowLeft}/>
+            </button>
+            <h1 className="profile__title">¡Hola {username}</h1>
+            <button className="profile__menu" aria-label="menú opciones del perfil">
+                <FontAwesomeIcon icon={faEllipsisVertical}/>
+            </button>
         </header>
         <section className="profile__container">
-
-        <Button variant = "primary">Guardar</Button>
+            <p className="profile__text">{username}</p>
+            <p className="profile__text">{email}</p>
+            <p className="profile__text">{password}</p>
         </section>
         <Footer />
         </>

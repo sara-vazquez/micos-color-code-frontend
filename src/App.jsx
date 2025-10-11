1bfbe2bc76c66321faada6958679a2ebff0ee530
Sara Vázquez <saravazlo@gmail.com>
2025-10-11 12:44:53 +0200

    fix: fixed app.jsx, cleaner

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router} from 'react-router-dom'
 import React from 'react'
 import "./components/styles/Variables.css";
 import './App.css'
-import HomePage from './pages/HomePage';
-import SystemPage from './pages/SystemPage';
-import DaltonismPage from './pages/DaltonismPage';
+import AppRouter from './router/AppRouter';
 
 
 function App() {
 
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/system" element={<SystemPage />} />
-        <Route path="/daltonism" element={<DaltonismPage />} />
-      </Routes>
+      <AppRouter />
     </Router>
   )
 }

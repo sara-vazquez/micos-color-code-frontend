29dcaeed598b96f92514dbe6e9b0bd676423db20
Sara Vázquez <saravazlo@gmail.com>
2025-10-11 12:24:09 +0200

    feat: created private route file

---
+++ src/router/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { isAuthenticated } from "../services/authService";
+
+export default function PrivateRoute({ children }) {
+    if (!isAuthenticated()) {
+        //if it's not authenticated, returns to login page -> protect private routes
+        return <Navigate to="/login" replace />;
+    }
+
+    return children;
+}

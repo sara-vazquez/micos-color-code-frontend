be580d634e7f4e909db01e2fc8d88728ec0f128c
Sara Vázquez <saravazlo@gmail.com>
2025-10-14 18:54:24 +0200

    style: added styles to play page

--- src/pages/letsPlayPage/LetsPlayPage.jsx
+++ src/pages/letsPlayPage/LetsPlayPage.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './LetsPlayPage.css';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 export default function LetsPlayPage() {
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        navigate(-1);
+    }
     return(
         <section className="play-page" id="top">
             <header className="play-page__intro">
                 <article className="play-page__header">
-                <button className="play-page-back" aria-label="botón para volver a la página anterior">
+                <button className="play-page-back" aria-label="botón para volver a la página anterior" onClick={handleBack}>
                     <FontAwesomeIcon icon={faArrowLeft} />
                 </button>
                 <h1 className="play-page__title">¡A jugar!</h1>

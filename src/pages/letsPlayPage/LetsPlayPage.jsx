5dd3cf7d48cc8c3dfae2fa20727039209c249c82
Sara Vázquez <saravazlo@gmail.com>
2025-10-14 18:41:31 +0200

    feat: created play page structure

---
+++ src/pages/letsPlayPage/LetsPlayPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './LetsPlayPage.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+
+export default function LetsPlayPage() {
+    return(
+        <section className="play-page" id="top">
+            <header className="play-page__intro">
+                <article className="play-page__header">
+                <button className="play-page-back" aria-label="botón para volver a la página anterior">
+                    <FontAwesomeIcon icon={faArrowLeft} />
+                </button>
+                <h1 className="play-page__title">¡A jugar!</h1>
+                </article>
+                <p className = "play-page__text">Aquí tienes una selección de juegos para poner en práctica lo aprendido y seguir mejorando cada día un poquito más.</p>
+            </header>
+            <main className="play-page__content">
+
+            </main>
+        </section>
+    );
+}

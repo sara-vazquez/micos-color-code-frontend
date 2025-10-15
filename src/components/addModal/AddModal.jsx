6a05757a5b9465120e751316082cda967f3b350d
Sara Vázquez <saravazlo@gmail.com>
2025-10-15 11:48:11 +0200

    feat: created addModal file to upload resources as admin

---
+++ src/components/addModal/AddModal.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './AddModal.css';
+import Button from '../buttons/Button';
+
+export default function AddModal({onCancel }) {
+    return(
+        <section className="add-modal">
+            <form className="add-modal__content">
+                <article className="add-modal__img-container" placeholder="Vista previa de tu archivo">
+                    <img className="add-modal__img" />
+                </article>
+                <article className="add-modal__input-group">
+                    <label className="add-modal__label">Nombre</label>
+                    <input className="add-modal__input" placeholder='Nombre del recurso'></input>
+
+                    <label className="add-modal__label">Introducción</label>
+                    <input className="add-modal__input" placeholder='Cuenta brevemente qué es el archivo'></input>
+
+                    <label className="add-modal__label">Descripción</label>
+                    <textarea className="add-modal__textarea" placeholder="Describe en qué consiste este recurso"></textarea>
+                
+                    <label className="add-modal__label" htmlFor={fileId}>Selecciona un archivo</label>
+                    <input className="add-modal__input" id={fileId} type="file" />
+                </article>
+                <article className="add-modal__actions">
+                    <Button type="button" onClick={onCancel} variant="secondary">Cancelar</Button>
+                    <Button type="submit" variant="primary">Guardar</Button>
+                </article>
+            </form>
+        </section>
+    );
+}

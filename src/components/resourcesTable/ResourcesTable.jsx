ac8fe7fc4d6a1ce7d205e11a33eec608d9645a31
Sara Vázquez <saravazlo@gmail.com>
2025-10-15 15:19:09 +0200

    feat: created admin resources table

---
+++ src/components/resourcesTable/ResourcesTable.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import './ResourcesTable.css';
+import { Pencil, Trash2 } from 'lucide-react';
+
+export default function ResourcesTable({data, onEdit, onDelete}) {
+    return(
+        <section className="resources-table__container">
+            <table className="resources-table">
+                <thread className="resources-table__thread">
+                    <th>Imagen</th>
+                    <th>PDF</th>
+                    <th>Nombre</th>
+                    <th>Intro</th>
+                    <th>Descripción</th>
+                    <th>Acciones</th>
+                </thread>
+                <tbody className='resources-table__body'>
+          {data.length === 0 ? (
+            <tr>
+              <td colSpan="6" className="resources-table--empty">
+                No hay recursos disponibles
+              </td>
+            </tr>
+          ) : (
+            data.map((resource) => (
+              <tr key={resource.id}>
+                <td className="resources-table__img-cell">
+                  <img 
+                    src={resource.image} 
+                    alt={resource.name}
+                    className="resources-table__img"
+                  />
+                </td>
+                <td className="resources-table__pdf-cell">
+                  <span className="resources-table__pdf">{resource.pdf}</span>
+                </td>
+                <td className="tresources-table__name-cell">
+                  {resource.nombre}
+                </td>
+                <td className="resources-table__intro-cell">
+                  {resource.intro}
+                </td>
+                <td className="resources-table__description-cell">
+                  {resource.description}
+                </td>
+                <td className="resources-table__actions-cell">
+                  <button onClick={() => onEdit(resource)} className="btn-edit" aria-label="Editar recurso">
+                    <Pencil size={24} />
+                  </button>
+                  <button onClick={() => onDelete(resource.id)} className="btn-delete" aria-label="Eliminar recurso">
+                    <Trash2 size={24} />
+                  </button>
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+            </table>
+        </section>
+    );
+}

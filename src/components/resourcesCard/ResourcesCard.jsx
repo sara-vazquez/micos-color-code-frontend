2b0ad1917f17f6a318a86abeaa640d2c93295be3
Sara Vázquez <saravazlo@gmail.com>
2025-10-17 19:04:09 +0200

    fix: fixed resources card with download logic and prepared to get data from back

--- src/components/resourcesCard/ResourcesCard.jsx
+++ src/components/resourcesCard/ResourcesCard.jsx
@@ -3,23 +3,30 @@ import './ResourcesCard.css';
 import { Eye, Download } from 'lucide-react';
 import ModalResources from '../modalResources/ModalResources';
 
-export default function ResourcesCard({ title, text }) {
+export default function ResourcesCard({ resource }) {
     const [openModalResources, setOpenModalResources] = useState(false);
 
-        if(openModalResources) return <ModalResources onClose={() => setOpenModalResources(false)}/>
+    const handleDownload = () => {
+        const link = document.createElement('a');
+        link.href = resource.pdfUrl;
+        link.download = resource.pdf;
+        link.click();
+    };
+
+        if(openModalResources) return <ModalResources resource={resource} onClose={() => setOpenModalResources(false)}/>
 
     return(
         <>
         <section className='resources-card__container'>
             <article className="daltonsim-card__content">
-                <h3 className='resources-card__h3'>{title}</h3>
-                <p className="resources-card__text">{text}</p>
+                <h3 className='resources-card__h3'>{resource.name}</h3>
+                <p className="resources-card__text">{resource.intro}</p>
             </article>
             <article className="resources-card__actions">
                 <button className="resources-card__icon" aria-label="Botón de vista previa del material" onClick={() => setOpenModalResources(true)}>
                     <Eye />
                 </button>
-                <button className="resources-card__icon" aria-label="Botón de descargar el material">
+                <button className="resources-card__icon" aria-label="Botón de descargar el material" onClick={handleDownload}>
                     <Download />
                 </button>
             </article>

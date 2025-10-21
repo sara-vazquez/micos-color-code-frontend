48b776cb9b33eb7e14d58fde617057ae3b51d108
Sara Vázquez <saravazlo@gmail.com>
2025-10-21 14:42:39 +0200

    fix: updated ranking chart structure

--- src/components/rankingChart/RankingChart.jsx
+++ src/components/rankingChart/RankingChart.jsx
@@ -5,16 +5,29 @@ import Button from '../buttons/Button';
 export default function RankingChart() {
     return(
         <div className="ranking-chart__overlay">
-            <section className='ranking-chart'>
-                <article className='ranking-chart__title'>
+            <main className='ranking-chart'>
+                <section className='ranking-chart__title'>
                     <h1>CLASIFICACIÓN</h1>
-                </article>
+                </section>
 
-                <article className="ranking-chart__actions">
+                <section className="ranking-chart__bar-group">
+                    <article className="ranking-chart__bar">
+                        <h3 className="ranking-chart__bar-username">{username}</h3>
+                        <p className="ranking-chart__bar-points">{points} puntos</p>
+                        <h1 className="ranking-chart__bar-position">{position}</h1>
+                    </article>
+                </section>
+                <section className="ranking-chart__personal-card">
+                    <p className="rankging-chart__personal-card--username">{username}</p>
+                    <p className="rankging-chart__personal-card--points">{points} puntos</p>
+                    <p className="rankging-chart__personal-card--position">{position}</p>
+                </section>
+
+                <section className="ranking-chart__actions">
                     <Button variant='primary'>JUGAR DE NUEVO</Button>
                     <Button variant='secondary'>VOLVER AL MENÚ</Button>
-                </article>
-            </section>
+                </section>
+            </main>
         </div>
     );
 }

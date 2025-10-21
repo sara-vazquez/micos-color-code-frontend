877e19b0a010930a3e0996671ed5e7f1da3beb7f
Sara Vázquez <saravazlo@gmail.com>
2025-10-21 15:16:28 +0200

    feat: added use navigate and navigation to play page

--- src/components/rankingChart/RankingChart.jsx
+++ src/components/rankingChart/RankingChart.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 import './RankingChart.css';
 import Button from '../buttons/Button';
 
 export default function RankingChart() {
+    const navigate = useNavigate();
+    const goToPlayPage = () => { navigate("/users/play"); }
+
     return(
         <div className="ranking-chart__overlay">
             <main className='ranking-chart'>
@@ -34,8 +38,8 @@ export default function RankingChart() {
                 </section>
 
                 <section className="ranking-chart__actions">
-                    <Button variant='primary'>JUGAR DE NUEVO</Button>
-                    <Button variant='secondary'>VOLVER AL MENÚ</Button>
+                    <Button type="button" variant='primary' aria-label="botón para volver a jugar">JUGAR DE NUEVO</Button>
+                    <Button type="button" variant='secondary' aria-label="botón para volver al menú de juegos" onClick={goToPlayPage}>VOLVER AL MENÚ</Button>
                 </section>
             </main>
         </div>

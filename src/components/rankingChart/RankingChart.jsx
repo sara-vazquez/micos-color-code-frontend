2e2dcc8458d7b35d8e70c0db74cbd00f746489ef
Sara Vázquez <saravazlo@gmail.com>
2025-10-22 09:31:55 +0200

    fix: ranking chart updated, need to update with use effect when backend's done

--- src/components/rankingChart/RankingChart.jsx
+++ src/components/rankingChart/RankingChart.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 import './RankingChart.css';
 import Button from '../buttons/Button';
 
 export default function RankingChart() {
+    const [rankgingChart, setRankingChart] = useState(null);
     const navigate = useNavigate();
     const goToPlayPage = () => { navigate("/users/play"); }
+    const gameId = 1;
+
+    //useEffect
 
     return(
         <div className="ranking-chart__overlay">
@@ -15,30 +19,24 @@ export default function RankingChart() {
                 </section>
 
                 <section className="ranking-chart__bar-group">
-                    <article className="ranking-chart__bar">
-                        <h3 className="ranking-chart__bar-username">{username}</h3>
-                        <p className="ranking-chart__bar-points">{points} puntos</p>
-                        <h1 className="ranking-chart__bar-position--1">{position}</h1>
-                    </article>
-                    <article className="ranking-chart__bar">
-                        <h3 className="ranking-chart__bar-username">{username}</h3>
-                        <p className="ranking-chart__bar-points">{points} puntos</p>
-                        <h1 className="ranking-chart__bar-position--2">{position}</h1>
-                    </article>
-                    <article className="ranking-chart__bar">
-                        <h3 className="ranking-chart__bar-username">{username}</h3>
-                        <p className="ranking-chart__bar-points">{points} puntos</p>
-                        <h1 className="ranking-chart__bar-position--3">{position}</h1>
-                    </article>
+                    {rankingChart.top3.map((player, index) => (
+                        <article key={player.username} className="ranking-chart__bar">
+                            <h3 className="ranking-chart__bar-username">{player.username}</h3>
+                            <p className="ranking-chart__bar-points">{player.totalPoints} puntos</p>
+                            <h1 className={`ranking-chart__bar-position--${index + 1}`}>
+                                {player.position}º
+                            </h1>
+                        </article>
+                    ))}
                 </section>
                 <section className="ranking-chart__personal-card">
-                    <p className="rankging-chart__personal-card--username">{username}</p>
-                    <p className="rankging-chart__personal-card--points">{points} puntos</p>
-                    <p className="rankging-chart__personal-card--position">{position}</p>
+                    <p className="rankging-chart__personal-card--username">{rankingChart.currentUser.username}</p>
+                    <p className="rankging-chart__personal-card--points">{rankingChart.currentUser.totalPoints} puntos</p>
+                    <p className="rankging-chart__personal-card--position">{rankingChart.currentUser.position}º</p>
                 </section>
 
                 <section className="ranking-chart__actions">
-                    <Button type="button" variant='primary' aria-label="botón para volver a jugar">JUGAR DE NUEVO</Button>
+                    <Button type="button" variant='primary' aria-label="botón para volver a jugar" onClick={() => navigate("/users/play/memory-cards")}>JUGAR DE NUEVO</Button>
                     <Button type="button" variant='secondary' aria-label="botón para volver al menú de juegos" onClick={goToPlayPage}>VOLVER AL MENÚ</Button>
                 </section>
             </main>

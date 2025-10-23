7c995cd1e280716356bb99e7253e4ee4dd9e3423
Sara Vázquez <saravazlo@gmail.com>
2025-10-23 09:20:20 +0200

    feat: finished game session service

--- src/services/gameSessionService.js
+++ src/services/gameSessionService.js
@@ -1,5 +1,24 @@
 const API_URL = "http://localhost:8080"
 
 export const gameSessionService = {
+    async completeSession(gameId, points, timeCompleted = null) {
+        const token = localStorage.getItem('token');
 
+        const response = await fetch(`${API_URL}/play/${gameId}/sessions`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer${token}`
+            },
+            body: JSON.stringify({points,timeCompleted})
+        });
+
+        if(!response.ok) {
+            if(response.status === 401) {
+                throw new Error('No autorizado. Por favor, inicia sesión nuevamente.');
+            }
+            throw new Error('Error al obtener el ranking');
+        }
+        return await response.json();
+    }
 }
